7943b41588f4d6bb6e7f1d988ec04803cf6b7c62
laisujie <laisujie@taqu.cn>
2020-08-08 10:33:13 +0800

    增加news接口

--- node-web/base-express/test/router/app.js
+++ node-web/base-express/test/router/app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 const skuRouter = require('./sku.router');
 const personRouter = require('./person.router');
+const newsRouter = require('./news.router');
 
 //引用路由中间件
 app.use('/sku',skuRouter);
 app.use('/person',personRouter);
+app.use('/news',newsRouter);
 
 //监听服务
 app.listen('3000',()=>{
