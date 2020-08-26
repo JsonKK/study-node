d6bc782d2ac536bb66ba3fdf7eced6ff88915b32
laisujie <laisujie@taqu.cn>
2020-08-26 15:29:38 +0800

    调试理解 await next();执行流程

--- node-web/base-koa/test/hello-world/app.js
+++ node-web/base-koa/test/hello-world/app.js
@@ -5,21 +5,26 @@ app.keys = ['name', 'tobi'];
 //再到这里
 // 日志
 app.use(async (ctx,next)=>{
+  console.log('---1---');
   await next();
+  console.log('---2---');
   const rt = ctx.response.get('X-Response-Time');
   console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 })
 
 //先走这里
 app.use(async (ctx,next)=>{
+  console.log('---3---');
   const start = new Date();
   await next();
+  console.log('---4---');
   const ms = Date.now() -start;
   ctx.set('X-Response-Time',`${ms}ms`);
 })
 
 //再到这里
 app.use(async ctx => {
+  console.log('---5---');
   ctx.cookies.set('name', 'tobi', { signed: true });
   ctx.body = 'Hello World';
 });
